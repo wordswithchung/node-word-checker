7f7971098a78272c2ecf6a0407454c0088fead81
Chung Nguyen <chung.nguyen@gmail.com>
2020-11-28 16:26:37 -0800

    Set up package.json and the app

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/checkWord/:word', (req, res) => {
+  console.log(req.params);
+  res.send('Hello World!')
+})
+
+app.get('/checkWords/:words', (req, res) => {
+  console.log(req.params);
+  res.send(true);
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
